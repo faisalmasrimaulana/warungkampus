cb190c7d07461cc8841f62024173918ee10f5a7c
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-06-10 06:20:37 +0700

    add:menambahkan route ke halaman bantuan

---
+++ app/Http/Controllers/BantuanController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Bantuan;
+use Illuminate\Support\Facades\Redis;
+
+class BantuanController extends Controller
+{
+    public function store(Request $request){
+        $validate = $request->validate([
+            'nama' => "required|string",
+            'email' => "required|email",
+            'detail_laporan' => "required|string",
+            'bukti' => "nullable|mimes:jpg,png,jpeg,pdf|max:5120"
+        ]);
+
+        $path = null;
+        if ($request->hasFile('bukti')) {
+        $path = $request->file('bukti')->store('bukti-laporan', 'public');
+        }
+
+        Bantuan::create([
+            'nama' => $validate['nama'],
+            'email' => $validate['email'],
+            'detail_laporan' => $validate['detail_laporan'],
+            'bukti' => $path,
+        ]);
+         return redirect()->route('bantuan')->with('success', 'Laporan berhasil dikirimkan');
+    }    
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,15 +44,17 @@
         </div>
       </a>
       
-      <div class="transition-all duration-300 ease-in-out bg-gradient-to-br from-white to-slate-50 p-8 hover:-translate-y-1 hover:shadow:xl shadow-lg rounded-2xl">
-        <div class="w-14 h-14 bg-purple-100 rounded-xl flex items-center justify-center mb-5">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
+      <a href="{{route('bantuan')}}">
+        <div class="transition-all duration-300 ease-in-out bg-gradient-to-br from-white to-slate-50 p-8 hover:-translate-y-1 hover:shadow:xl shadow-lg rounded-2xl">
+          <div class="w-14 h-14 bg-purple-100 rounded-xl flex items-center justify-center mb-5">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+          </div>
+          <h3 class="text-xl font-semibold mb-3 text-gray-800">Hubungi Kami</h3>
+          <p class="text-gray-600">Punya pertanyaan atau laporan? Kami siap membantu.</p>
         </div>
-        <h3 class="text-xl font-semibold mb-3 text-gray-800">Hubungi Kami</h3>
-        <p class="text-gray-600">Punya pertanyaan atau laporan? Kami siap membantu.</p>
-      </div>
+      </a>
     </section>
     
     <!-- Testimonial Section -->
